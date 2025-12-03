86f26d85191168b31e0da996c5a3a3b4827704f2
My Home <My Home@DESKTOP-6CB6R1F>
2025-12-04 00:48:40 +0900

    333

--- Global.asax.cs
+++ Global.asax.cs
@@ -18,7 +18,8 @@ namespace MvcMovie2
         void Application_Start(object sender, EventArgs e)
         {
             // ★ DB가 없거나 모델이 바뀌면 기존 DB를 날리고 새로 만듭니다. (Migration 안 씀)
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<MovieListContext>());
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<MovieListContext>());
+            Database.SetInitializer<MovieListContext>(null);
 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
