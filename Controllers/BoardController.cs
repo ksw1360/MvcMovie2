0a5f8c23e69da78ae2206b624556233b84456d7e
My Home <My Home@DESKTOP-6CB6R1F>
2025-12-04 00:58:18 +0900

    계속 변경중.....20251204

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie2.Controllers
+{
+    public class BoardController : Controller
+    {
+        // GET: Board
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
